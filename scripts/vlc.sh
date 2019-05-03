740550f484f0cc2cec81df1a179d61404f5780ed
Bill Torpey <wallstprog@gmail.com>
2019-05-03 15:58:55 -0400

    initial checkin

---
+++ scripts/vlc.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+#   Copyright 2015 Itiviti AB
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+#
+# Format & filter valgrind leak reports
+#
+# Usage: vlc.sh [-f] [-m] [-i] [-r] [-p] [-t] [-d] {files}
+#
+# -f Filter output to contain only relevant stack traces.  Default is to include
+# all stack frames.
+#
+# -m Only report multiple leaks (count > 1).  Default is to include single leaks as well.
+#
+# -i Include "indirectly lost" blocks.
+#
+# -r Include "still reachable" blocks.
+#
+# -p Include "possibly lost" blocks.
+#
+# -d Output debug messages.
+#
+# -v Verbose output.  (Report even if all leaks suppressed).
+#
+# NOTE: "definitely lost" blocks are always included.
+#
+# -t Process input files in sorted order based on timestamp.
+# Default is to assume input files are named in the form xxxxxxx-<pid>.* and to sort files
+# numerically by the value of <pid>, which is typically the order in which processes are started,
+# and should therefore facilitate comparing different runs.
+#
+# NOTE:
+# requires gawk 4.1 or later
+# for Mac, need to install gnu awk (gawk) -- following worked for me:
+#   brew install gawk
+
+# get current directory
+SCRIPT_DIR=$(cd `dirname $BASH_SOURCE` && pwd)
+export AWKPATH=${SCRIPT_DIR}:${AWKPATH}
+
+FILTER=0
+MULTI=0
+REACHABLE=0
+INDIRECT=0
+POSSIBLY=0
+TIMESORT=0
+DEBUG=0
+VERBOSE=0
+while getopts 'fmriptdv' flag; do
+  case "${flag}" in
+    f) FILTER=1 ;;
+    m) MULTI=1 ;;
+    r) REACHABLE=1 ;;
+    i) INDIRECT=1 ;;
+    p) POSSIBLY=1 ;;
+    t) TIMESORT=1 ;;
+    d) DEBUG=1 ;;
+    v) VERBOSE=1 ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+if [[ ${OSTYPE} == *darwin* ]]; then
+  MD5SUM="md5"
+else
+  MD5SUM="md5sum"
+fi
+
+if [[ ${TIMESORT} == 1 ]]; then
+  FILES=$(ls -tr1 $*)
+else
+  FILES=$(ls -1 $* | sort -n --field-separator=- --key=2,2)
+fi
+
+#echo 'FILES=' $FILES 1>&2
+
+# uncomment to get lint output
+#LINT="--lint"
+
+RC=0
+ERRORS=0
+FAILS=0
+# loop thru files specified on command line
+for filename in $FILES; do
+   # run the awk script to format, filter, etc.
+   OUTPUT=$(gawk -f $SCRIPT_DIR/vlc.awk ${LINT} -v debug=${DEBUG} -v md5sum=${MD5SUM} -v filter=${FILTER} -v multi=${MULTI} -v reachable=${REACHABLE} -v indirect=${INDIRECT} -v possibly=${POSSIBLY} -v timesort=${TIMESORT} -v keepFile="${SCRIPT_DIR}/vlc.keep" -v discardFile="${SCRIPT_DIR}/vlc.supp" $filename)
+   RC=$?
+   # set flags
+   [[ ($RC == 1) ]] && ERRORS=1
+   if [[ ($RC == 2) ]]; then
+      echo "Script failed -- parsing error in $filename!" 1>&2
+      FAILS=1
+   fi
+   # print output
+   if [[ ($RC != 0) || (${VERBOSE} == 1) ]]; then
+      # get the process name
+      echo "==============================="
+      echo -n "File="$(basename $filename)
+      echo -n ", Program="
+      grep 'Command:' $filename|awk '{s = ""; for (i = 3; i <= NF; i++) s = s $i " "; printf "%s", s }'
+      echo ""
+      echo "$OUTPUT"
+      echo
+   fi
+   #echo
+done
+
+if [[ $FAILS == 1 ]]; then
+   echo "Script failed -- parsing error(s) reported!" 1>&2
+   exit 2
+fi
+
+if [[ $ERRORS == 1 ]]; then
+   echo "Errors reported" 1>&2
+   exit 1
+fi
+
+exit 0
